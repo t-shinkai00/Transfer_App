986e840ad25eb148594439718e92c479eb52c651
t-shinkai00 <t.shinkai00@gmail.com>
2022-10-28 04:01:59 +0900

    update

--- src/components/Prefecture.ts
+++ src/components/Prefecture.ts
@@ -3,6 +3,16 @@ interface Prefecture {
   name: string;
 }
 
+const compareWith = (o1: Prefecture, o2: Prefecture) => {
+  if (!o1 || !o2) {
+    return o1 === o2;
+  }
+  if (Array.isArray(o2)) {
+    return o2.some((o) => o.id === o1.id);
+  }
+  return o1.id === o2.id;
+};
+
 const prefectures: Prefecture[] = [
   {
     id: 1,
@@ -193,15 +203,5 @@ const prefectures: Prefecture[] = [
     name: "沖縄県",
   },
 ];
-
-const compareWith = (o1: Prefecture, o2: Prefecture) => {
-  if (!o1 || !o2) {
-    return o1 === o2;
-  }
-  if (Array.isArray(o2)) {
-    return o2.some((o) => o.id === o1.id);
-  }
-  return o1.id === o2.id;
-};
 
 export { prefectures, compareWith };
