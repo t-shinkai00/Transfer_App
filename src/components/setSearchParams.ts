894c3631bc27031eda3e1d79952a13fceda1ec5e
t-shinkai00 <t.shinkai00@gmail.com>
2022-10-28 12:59:53 +0900

    add: setSearchParams function

---
+++ src/components/setSearchParams.ts
@@ -0,0 +1,33 @@
+import StationSearch from "../../interfaces/StationSearchInterface";
+
+export const setStationSearchParams = (
+  key: string | undefined,
+  name: string | null = null,
+  oldName: string | null = null,
+  corporationName: string | null = null,
+  railName: string | null = null,
+  type: string | null = null,
+  prefectureCode: string | null = null,
+  addGateGroup: boolean = false
+) => {
+  const params: StationSearch = {
+    key: key,
+    name: name,
+    oldName: oldName,
+    corporationName: corporationName,
+    railName: railName,
+    type: type,
+    prefectureCode: prefectureCode,
+    addGateGroup: addGateGroup,
+  };
+
+  if (name === "") delete params.name;
+  if (oldName === "") delete params.oldName;
+  if (corporationName === "") delete params.corporationName;
+  if (railName === "") delete params.railName;
+  if (type === "") delete params.type;
+  if (prefectureCode === "") delete params.prefectureCode;
+  if (addGateGroup === false) delete params.addGateGroup;
+
+  return params;
+};
